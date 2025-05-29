5d14a2dc61be0a6f81266be7e5bcd329b79a0a52
HyunJinNo <nhj9831@naver.com>
2025-05-29 10:41:17 +0900

    chore: prettier-plugin-tailwindcss 설정

--- src/pages/home/ui/HomeScreen.tsx
+++ src/pages/home/ui/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, Text, View } from 'react-native';
+import { tw } from '../../../shared/lib/utils';
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
     <View>
-      <Text>Home</Text>
+      <Text style={tw`ml-5 mt-10 text-blue-500`}>Home</Text>
       <Button
         title="설정화면으로"
         onPress={() => navigation.navigate('Setting')}
