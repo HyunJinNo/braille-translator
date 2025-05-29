ef603ae703736decf75f57764714f8d8bfa3c35e
HyunJinNo <nhj9831@naver.com>
2025-05-29 15:27:13 +0900

    chore: React Native 캐시 삭제 Script 추가

--- src/pages/home/ui/HomeScreen.tsx
+++ src/pages/home/ui/HomeScreen.tsx
@@ -1,13 +1,12 @@
 import { useNavigation } from '@react-navigation/native';
 import { Button, Text, View } from 'react-native';
-import { tw } from '../../../shared/lib/utils';
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
     <View>
-      <Text style={tw`ml-5 mt-10 text-blue-500`}>Home</Text>
+      <Text>Home</Text>
       <Button
         title="설정화면으로"
         onPress={() => navigation.navigate('Setting')}
