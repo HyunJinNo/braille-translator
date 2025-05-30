99df12d15e8aa99750ea64fe9055fca721a8f9e1
HyunJinNo <nhj9831@naver.com>
2025-05-30 14:42:02 +0900

    chore: Path Alias 적용

--- src/pages/home/ui/HomeScreen.tsx
+++ src/pages/home/ui/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { BACKEND_URL } from '@env';
 import { useNavigation } from '@react-navigation/native';
-import { Button, Text, View } from 'react-native';
+import { tw } from '@src/shared/lib/utils';
+import { Button, Image, Text, View } from 'react-native';
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
@@ -9,6 +10,10 @@ export const HomeScreen = () => {
     <View>
       <Text>Home</Text>
       <Text>{BACKEND_URL}</Text>
+      <Image
+        style={tw`aspect-2/3 h-96`}
+        source={require('@assets/background.jpg')}
+      />
       <Button
         title="설정화면으로"
         onPress={() => navigation.navigate('Setting')}
