90cd5d785ac3d0e4d0d4572fc7471996b27e917f
HyunJinNo <nhj9831@naver.com>
2025-05-31 16:07:34 +0900

    feat: 스플래시 화면 구현

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import { Navigation } from './routes';
+import SplashScreen from 'react-native-splash-screen';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return <Navigation />;
 };
