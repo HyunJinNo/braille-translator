8e8e59959e6581f93c61efc95e714bee8e86b4eb
HyunJinNo <nhj9831@naver.com>
2025-06-01 21:04:36 +0900

    design: 홈 화면 UI 구현

--- src/pages/home/ui/HomeScreen.tsx
+++ src/pages/home/ui/HomeScreen.tsx
@@ -1,22 +1,56 @@
-import { BACKEND_URL } from '@env';
-import { useNavigation } from '@react-navigation/native';
 import { tw } from '@src/shared/lib/utils';
-import { Button, Image, Text, View } from 'react-native';
+import { HomeButton } from '@src/shared/ui/button';
+import { FlatList, View } from 'react-native';
+
+const data = [
+  {
+    source: require('@assets/icons/hangul-scan-icon.png'),
+    title: '한글→점자',
+    description: ['카메라로 스캔한', '한글을 점자로', '번역합니다.'],
+    buttonStyle: 'border-fuchsia-200 bg-fuchsia-50',
+    textColor: 'text-fuchsia-400',
+  },
+  {
+    source: require('@assets/icons/braille-scan-icon.png'),
+    title: '한글→점자',
+    description: ['카메라로 스캔한', '점자를 한글로', '번역합니다.'],
+    buttonStyle: 'border-green-200 bg-green-50',
+    textColor: 'text-green-400',
+  },
+  {
+    source: require('@assets/icons/hangul-to-braille-icon.png'),
+    title: '한글→점자',
+    description: ['사진 내', '한글을 점자로', '번역합니다.'],
+    buttonStyle: 'border-blue-200 bg-blue-50',
+    textColor: 'text-blue-400',
+  },
+  {
+    source: require('@assets/icons/braille-to-hangul-icon.png'),
+    title: '한글→점자',
+    description: ['사진 내', '점자를 한글로', '번역합니다.'],
+    buttonStyle: 'border-cyan-200 bg-cyan-50',
+    textColor: 'text-cyan-400',
+  },
+];
 
 export const HomeScreen = () => {
-  const navigation = useNavigation();
-
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>{BACKEND_URL}</Text>
-      <Image
-        style={tw`aspect-2/3 h-96`}
-        source={require('@assets/background.jpg')}
-      />
-      <Button
-        title="설정화면으로"
-        onPress={() => navigation.navigate('Setting')}
+    <View style={tw`flex h-full flex-col items-center justify-center bg-white`}>
+      <FlatList
+        contentContainerStyle={tw`flex h-full min-w-full flex-col justify-center gap-5 px-4`}
+        columnWrapperStyle={tw`gap-5`}
+        data={data}
+        renderItem={({ item }) => (
+          <HomeButton
+            source={item.source}
+            title={item.title}
+            description={item.description}
+            buttonStyle={item.buttonStyle}
+            textColor={item.textColor}
+          />
+        )}
+        keyExtractor={(item) => item.description.join(' ')}
+        numColumns={2}
       />
     </View>
   );
---
+++ src/shared/ui/button/HomeButton.tsx
@@ -0,0 +1,58 @@
+import { tw } from '@src/shared/lib/utils';
+import {
+  FlatList,
+  GestureResponderEvent,
+  Image,
+  ImageSourcePropType,
+  Pressable,
+  Text,
+} from 'react-native';
+
+interface HomeButtonProps {
+  source: ImageSourcePropType;
+  title: string;
+  description: string[];
+  buttonStyle?: string;
+  textColor?: string;
+  onPress?: ((event: GestureResponderEvent) => void) | null;
+}
+
+export const HomeButton = ({
+  source,
+  title,
+  description,
+  buttonStyle,
+  textColor,
+  onPress,
+}: HomeButtonProps) => {
+  return (
+    <Pressable
+      style={tw.style(
+        buttonStyle ?? 'border-fuchsia-200 bg-fuchsia-50',
+        'h-75 flex flex-1 flex-col items-center rounded-2xl border pt-12 shadow',
+      )}
+      onPress={onPress}>
+      <Image style={tw`aspect-square h-20`} source={source} />
+      <Text
+        style={tw.style(
+          textColor ?? 'text-fuchsia-400',
+          'pt-4 text-center text-base font-semibold',
+        )}>
+        {title}
+      </Text>
+      <FlatList
+        style={tw`pt-6`}
+        data={description}
+        renderItem={({ item }) => (
+          <Text
+            style={tw.style(
+              textColor ?? 'text-fuchsia-400',
+              'text-center text-sm',
+            )}>
+            {item}
+          </Text>
+        )}
+      />
+    </Pressable>
+  );
+};
