5c813942afb3f2200afb69be06f80b64de31ab7c
HyunJinNo <nhj9831@naver.com>
2025-06-02 21:05:10 +0900

    design: 카메라 화면 추가

--- src/pages/translation/ui/TranslationScreen.tsx
+++ src/pages/translation/ui/TranslationScreen.tsx
@@ -1,13 +1,40 @@
+import { useNavigation } from '@react-navigation/native';
 import { tw } from '@src/shared/lib/utils';
 import { ControlBar } from '@src/widgets/controlBar';
 import { TranslationTextViewer } from '@src/widgets/translationTextViewer';
+import { useEffect } from 'react';
 import { View } from 'react-native';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 
 export const TranslationScreen = () => {
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!hasPermission) {
+      (async () => {
+        const cameraPermission = await requestPermission();
+
+        if (!cameraPermission) {
+          navigation.goBack();
+        }
+      })();
+    }
+  }, [hasPermission, navigation, requestPermission]);
+
   return (
-    <View>
-      <View style={tw`h-full bg-black`} />
-      <View style={tw`absolute bottom-0 z-10 w-full bg-white px-4`}>
+    <View style={tw`flex h-full flex-col justify-between bg-white`}>
+      {!device ? (
+        <View style={tw`w-full flex-1 bg-black`} />
+      ) : (
+        <Camera style={tw`w-full flex-1`} device={device} isActive={true} />
+      )}
+      <View style={tw`px-4`}>
         <ControlBar />
         <TranslationTextViewer />
       </View>
