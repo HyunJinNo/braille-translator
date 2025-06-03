ffcbf8114a5c5f3ed9a3bb8bfd692eb0ef4f2e8f
HyunJinNo <nhj9831@naver.com>
2025-06-03 14:44:41 +0900

    design: Bounding Box 추가

--- src/pages/translation/ui/TranslationScreen.tsx
+++ src/pages/translation/ui/TranslationScreen.tsx
@@ -3,7 +3,7 @@ import { tw } from '@src/shared/lib/utils';
 import { ControlBar } from '@src/widgets/controlBar';
 import { TranslationTextViewer } from '@src/widgets/translationTextViewer';
 import { useEffect } from 'react';
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import {
   Camera,
   useCameraDevice,
@@ -32,7 +32,16 @@ export const TranslationScreen = () => {
       {!device ? (
         <View style={tw`w-full flex-1 bg-black`} />
       ) : (
-        <Camera style={tw`w-full flex-1`} device={device} isActive={true} />
+        <View style={tw`flex-10 relative`}>
+          <Camera style={tw`h-full w-full`} device={device} isActive={true} />
+          <View
+            style={tw`absolute left-4 right-4 top-1/2 h-24 -translate-y-12 rounded-2xl border-2 border-white`}
+          />
+          <Text
+            style={tw`absolute left-0 right-0 top-1/2 translate-y-14 text-center text-base text-white`}>
+            텍스트를 박스에 비춰 주세요
+          </Text>
+        </View>
       )}
       <View style={tw`px-4`}>
         <ControlBar />
