e0f2a7b9fe059681f9fcde74ea349bac41a6b9f3
HyunJinNo <nhj9831@naver.com>
2025-06-04 20:18:18 +0900

    feat: 이미지 내 한글 인식 기능 구현

--- src/widgets/translationTextViewer/ui/TranslationTextViewer.tsx
+++ src/widgets/translationTextViewer/ui/TranslationTextViewer.tsx
@@ -1,14 +1,34 @@
 import { tw } from '@src/shared/lib/utils';
 import { ScrollView, Text, View } from 'react-native';
 
-export const TranslationTextViewer = () => {
+interface TranslationTextViewerProps {
+  recognizedText: string;
+  translatedText: string;
+}
+
+export const TranslationTextViewer = ({
+  recognizedText,
+  translatedText,
+}: TranslationTextViewerProps) => {
   return (
     <View style={tw`bg-white`}>
       <ScrollView style={tw`h-40 border-b border-t border-gray-400`}>
-        <Text style={tw`pt-2 text-xl text-gray-400`}>인식된 글자</Text>
+        <Text
+          style={tw.style(
+            recognizedText === '' && 'text-gray-400',
+            'pt-2 text-xl',
+          )}>
+          {recognizedText !== '' ? recognizedText : '인식된 글자'}
+        </Text>
       </ScrollView>
       <ScrollView style={tw`h-40`}>
-        <Text style={tw`pt-2 text-xl text-gray-400`}>번역된 글자</Text>
+        <Text
+          style={tw.style(
+            translatedText === '' && 'text-gray-400',
+            'pt-2 text-xl',
+          )}>
+          {translatedText !== '' ? translatedText : '번역된 글자'}
+        </Text>
       </ScrollView>
     </View>
   );
