7925125069bca508873953e87381ac45a36c96ac
HyunJinNo <nhj9831@naver.com>
2025-06-07 16:28:45 +0900

    feat: HistoryItem 컴포넌트 구현

---
+++ src/entities/history/ui/HistoryItem.tsx
@@ -0,0 +1,50 @@
+import { COLOR } from '@src/shared/config';
+import { tw } from '@src/shared/lib/utils';
+import { Image, Pressable, Text, View } from 'react-native';
+
+interface HistoryItemProps {
+  recognizedText: string;
+  translatedText: string;
+  createdAt: string;
+  isBookmarked: boolean;
+}
+
+export const HistoryItem = ({
+  recognizedText,
+  translatedText,
+  createdAt,
+  isBookmarked,
+}: HistoryItemProps) => {
+  return (
+    <Pressable
+      style={tw`flex w-full flex-row items-center justify-between gap-4 rounded-2xl border border-gray-200 bg-white p-4 shadow`}
+      android_ripple={{ color: COLOR['green-200'] }}>
+      <View style={tw`flex flex-1 flex-row items-center gap-4`}>
+        <Image
+          style={tw`h-18 w-18 rounded-xl`}
+          source={require('@assets/images/example.png')}
+        />
+        <View style={tw`flex-1 gap-1`}>
+          <Text
+            style={tw`text-base font-semibold`}
+            numberOfLines={1}
+            ellipsizeMode="tail">
+            {recognizedText}
+          </Text>
+          <Text style={tw`text-base`} numberOfLines={1} ellipsizeMode="tail">
+            {translatedText}
+          </Text>
+          <Text style={tw`text-xs text-green-400`}>{createdAt}</Text>
+        </View>
+      </View>
+      <Image
+        style={tw`h-10 w-10`}
+        source={
+          isBookmarked
+            ? require('@assets/icons/star-icon-filled-active.png')
+            : require('@assets/icons/star-icon-outline-inactive.png')
+        }
+      />
+    </Pressable>
+  );
+};
--- src/pages/history/ui/HangulToBrailleHistoryScreen.tsx
+++ src/pages/history/ui/HangulToBrailleHistoryScreen.tsx
@@ -1,14 +1,62 @@
+import { HistoryItem } from '@src/entities/history';
 import { tw } from '@src/shared/lib/utils';
 import { BookmarkButton } from '@src/shared/ui/button';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+
+// TODO: 삭제 필요
+const temp = [
+  {
+    recognizedText: '남자장애인화장실',
+    translatedText: '⠉⠢⠨⠨⠶⠗⠟⠚⠧⠨⠶⠠⠕⠂',
+    createdAt: '2025-06-06 21:05',
+    isBookmarked: true,
+  },
+  {
+    recognizedText: '실시간 점자 번역기 앱의 디자인입니다.',
+    translatedText: '⠠⠕⠂⠠⠕⠫⠒⠀⠨⠎⠢⠨⠀⠘⠾⠱⠁⠈⠕⠀⠗⠃⠺⠀⠊⠕⠨⠣⠟⠕⠃⠉⠕⠊⠲',
+    createdAt: '2025-06-06 21:04',
+    isBookmarked: false,
+  },
+  {
+    recognizedText: '남자장애인화장실',
+    translatedText: '⠉⠢⠨⠨⠶⠗⠟⠚⠧⠨⠶⠠⠕⠂',
+    createdAt: '2025-06-06 21:03',
+    isBookmarked: true,
+  },
+  {
+    recognizedText: '실시간 점자 번역기 앱의 디자인입니다.',
+    translatedText: '⠠⠕⠂⠠⠕⠫⠒⠀⠨⠎⠢⠨⠀⠘⠾⠱⠁⠈⠕⠀⠗⠃⠺⠀⠊⠕⠨⠣⠟⠕⠃⠉⠕⠊⠲',
+    createdAt: '2025-06-06 21:02',
+    isBookmarked: false,
+  },
+  {
+    recognizedText: '실시간 점자 번역기 앱의 디자인입니다.',
+    translatedText: '⠠⠕⠂⠠⠕⠫⠒⠀⠨⠎⠢⠨⠀⠘⠾⠱⠁⠈⠕⠀⠗⠃⠺⠀⠊⠕⠨⠣⠟⠕⠃⠉⠕⠊⠲',
+    createdAt: '2025-06-06 21:01',
+    isBookmarked: false,
+  },
+];
 
 export const HangulToBrailleHistoryScreen = () => {
   return (
-    <View style={tw`flex h-full flex-col bg-white px-4 pt-8`}>
+    <View style={tw`flex h-full flex-col gap-4 bg-white px-4 pb-4 pt-8`}>
       <View style={tw`flex flex-row items-center justify-between`}>
         <Text style={tw`text-lg font-semibold`}>목록</Text>
         <BookmarkButton />
       </View>
+      <FlatList
+        contentContainerStyle={tw`gap-4`}
+        data={temp}
+        renderItem={({ item }) => (
+          <HistoryItem
+            recognizedText={item.recognizedText}
+            translatedText={item.translatedText}
+            createdAt={item.createdAt}
+            isBookmarked={item.isBookmarked}
+          />
+        )}
+        keyExtractor={(item) => item.createdAt}
+      />
     </View>
   );
 };
