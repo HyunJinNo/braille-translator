b9bbceaf05b977fcb01569cca439f77ff6313d3f
HyunJinNo <nhj9831@naver.com>
2025-06-17 16:19:32 +0900

    feat: 번역 기록 상세 조회 기능 구현

--- src/entities/history/ui/HistoryItem.tsx
+++ src/entities/history/ui/HistoryItem.tsx
@@ -2,6 +2,8 @@ import { COLOR } from '@src/shared/config';
 import { tw } from '@src/shared/lib/utils';
 import { Image, Pressable, Text, View } from 'react-native';
 import { History } from '../model/history';
+import { useModal } from '@src/shared/lib/hooks';
+import { HistoryModal } from './HistoryModal';
 
 interface HistoryItemProps {
   history: History;
@@ -14,44 +16,55 @@ export const HistoryItem = ({
   onStarClick,
   onDeleteButtonPress,
 }: HistoryItemProps) => {
+  const { isOpen, openModal, closeModal } = useModal();
+
   return (
-    <Pressable
-      style={tw`flex w-full flex-row items-center justify-between gap-4 rounded-2xl border border-gray-200 bg-white p-4 shadow`}
-      android_ripple={{ color: COLOR['green-200'] }}>
-      <Pressable onPress={onStarClick}>
-        <Image
-          style={tw`h-10 w-10`}
-          source={
-            history.isBookmarked
-              ? require('@assets/icon/star-icon-filled-active.png')
-              : require('@assets/icon/star-icon-outline-inactive.png')
-          }
-        />
-      </Pressable>
-      <View style={tw`flex-1 gap-1`}>
-        <Text
-          style={tw`text-base font-semibold`}
-          numberOfLines={1}
-          ellipsizeMode="tail">
-          {history.recognizedText}
-        </Text>
-        <Text style={tw`text-base`} numberOfLines={1} ellipsizeMode="tail">
-          {history.translatedText}
-        </Text>
-        <Text style={tw`text-xs text-green-400`}>{history.createdAt}</Text>
-      </View>
-      <Pressable onPress={onDeleteButtonPress}>
-        {({ pressed }) => (
+    <View>
+      <HistoryModal
+        recognizedText={history.recognizedText}
+        translatedText={history.translatedText}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
+      <Pressable
+        style={tw`flex w-full flex-row items-center justify-between gap-4 rounded-2xl border border-gray-200 bg-white p-4 shadow`}
+        android_ripple={{ color: COLOR['green-200'] }}
+        onPress={openModal}>
+        <Pressable onPress={onStarClick}>
           <Image
             style={tw`h-10 w-10`}
             source={
-              pressed
-                ? require('@assets/icon/delete-icon-filled-active.png')
-                : require('@assets/icon/delete-icon-outline-inactive.png')
+              history.isBookmarked
+                ? require('@assets/icon/star-icon-filled-active.png')
+                : require('@assets/icon/star-icon-outline-inactive.png')
             }
           />
-        )}
+        </Pressable>
+        <View style={tw`flex-1 gap-1`}>
+          <Text
+            style={tw`text-base font-semibold`}
+            numberOfLines={1}
+            ellipsizeMode="tail">
+            {history.recognizedText}
+          </Text>
+          <Text style={tw`text-base`} numberOfLines={1} ellipsizeMode="tail">
+            {history.translatedText}
+          </Text>
+          <Text style={tw`text-xs text-green-400`}>{history.createdAt}</Text>
+        </View>
+        <Pressable onPress={onDeleteButtonPress}>
+          {({ pressed }) => (
+            <Image
+              style={tw`h-10 w-10`}
+              source={
+                pressed
+                  ? require('@assets/icon/delete-icon-filled-active.png')
+                  : require('@assets/icon/delete-icon-outline-inactive.png')
+              }
+            />
+          )}
+        </Pressable>
       </Pressable>
-    </Pressable>
+    </View>
   );
 };
