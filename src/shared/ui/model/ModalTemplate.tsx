3d0dc1beeb1c5ee6c967447f5d017c15c0f577ec
HyunJinNo <nhj9831@naver.com>
2025-06-17 10:54:48 +0900

    feat: ModalTemplate 컴포넌트 구현

---
+++ src/shared/ui/model/ModalTemplate.tsx
@@ -0,0 +1,46 @@
+import { tw } from '@src/shared/lib/utils';
+import { Image, Modal, Pressable, Text, View } from 'react-native';
+
+interface ModalTemplateProps {
+  children: React.ReactNode;
+  title: string;
+  visible: boolean;
+  closeModal: () => void;
+}
+
+export const ModalTemplate = ({
+  children,
+  title,
+  visible,
+  closeModal,
+}: ModalTemplateProps) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={() => closeModal()}>
+      <View style={tw`flex-1 items-center justify-center bg-black/50 px-4`}>
+        <View
+          style={tw`flex w-full flex-col items-center rounded-2xl bg-white p-4`}>
+          <View style={tw`flex w-full flex-row items-center justify-between`}>
+            <Text style={tw`text-lg font-semibold`}>{title}</Text>
+            <Pressable onPress={() => closeModal()}>
+              {({ pressed }) => (
+                <Image
+                  style={tw`h-6 w-6`}
+                  source={
+                    pressed
+                      ? require('@assets/icon/close-icon-active.png')
+                      : require('@assets/icon/close-icon-inactive.png')
+                  }
+                />
+              )}
+            </Pressable>
+          </View>
+          {children}
+        </View>
+      </View>
+    </Modal>
+  );
+};
